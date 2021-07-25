9c0d27318f10dd8b2c51b526fb66e5dba7c68067
brent <b.hizzes@gmail.com>
2021-07-25 14:45:51 +0100

    intro

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,10 +1,6 @@
 class Intro {
   idColor = 'green'
-  micColor = 'green';
   micDuration = 3;
-  measurementInterval = 25;
-  minFreq = 50;
-  maxFreq = 500;
 
   constructor(manager, doc, div, audio, share, status) {
     const that = this;
@@ -15,14 +11,15 @@ class Intro {
     let label = doc.create('label', "Thanks for agreeing to participate! Your anonymous identification number is ",div);
     label = doc.create('label', share.get('id'),div);
     label.style.color = this.idColor;
-    doc.create('label','. Please keep this for your records. If you would like to withdraw from the study, just tell us with this id number', div)
+    doc.create('label','. Please keep this for your records. If you would like to withdraw from the study, just tell us with this id number.', div)
     doc.create('hr',null,div);
     doc.create('label',"For this experiment, you will need to be able to hear audio and create audio.", div)
-    doc.create('hr',null,div);
 
+    let list = doc.create('ul', null, div);
     //audio check
-    doc.create('label', 'First, let\'s check your audio. When you click on the following button, you should hear some audio corresponding to someone saying "Hello" in Cantonese: ', div);
-    const audioButton = doc.create('button', 'Play!',div);
+    let audioLi = doc.create('li', null, list);
+    doc.create('label', 'When you click on the following button, you should hear some audio corresponding to someone saying "Hello" in Cantonese: ', audioLi);
+    const audioButton = doc.create('button', 'Play!',audioLi);
     audioButton.onclick = function() {
       audioButton.disabled = true;
       let a = audio.hello();
@@ -31,61 +28,24 @@ class Intro {
       }
       a.play();
     }
-    doc.create('hr',null,div);
 
     //mic check
-    doc.create('label', 'Now, let\'s check your microphone. Click the button, allow access to your microphone, and say "Hello" with ' + this.micDuration + ' seconds. You should see numbers appear to the right in ',div);
-    let colorLabel = doc.create('label', this.micColor, div);
-    colorLabel.style.color = this.micColor;
-    doc.create('label', ': ', div);
-    this.micButton = doc.create('button','Record', div);
-    this.micButton.onclick = getAudioStream(function(audioContext, stream) {that.record(audioContext,stream)});
-    this.micLabel = doc.create('label','', div);
-    this.micLabel.style.color = this.micColor;
-    doc.create('hr',null,div);
+    let micLi = doc.create('li', null, list);
+    let gain = this.share.get('micGain'); 
+    if(gain == null) this.share.save('micGain',1);
+    let volFn = function() {return that.share.get('micGain');};
+    let pb = definePlayback(doc, this.micDuration, null, function() {}, function() {}, function() {}, volFn)
+    doc.create('label', 'Now, click the "Record" button, allow access to your microphone, and say something within ' + this.micDuration + ' seconds. You should be able to play what you recorded with the "Play Attempt" button and to adjust playback volume with the "Up" and "Down" buttons ',micLi);
+    micLi.appendChild(pb['record']);
+    micLi.appendChild(pb['playback']);
+    getVolButtons(doc, this.share, micLi);
 
     //next
     this.nextButton = doc.create('button','Move on to next step!', div);
-    this.nextButton.style.display = 'none';
-    this.nextButton.onclick = function() {manager.next();};
-  }
-
-  ready() {
-    this.nextButton.style.display = 'block';
-  }
-
-  record(audioContext, stream) {
-    const that = this;
-
-    this.share.save('audioContext', audioContext);
-    this.share.save('stream',stream)
-
-    this.micButton.disabled = true;
-
-    let analyzer = getAnalyzer(audioContext, stream);
-    const sampleRate = audioContext.sampleRate;
-
-    const data = new Float32Array(analyzer.fftSize);
-
-    var idx = 0;
-
-    const stop = function() {
-      that.micButton.disabled = false;
-      that.share.clearTimeouts();
-    }
-
-    const intervalFn = function() {
-      analyzer.getFloatTimeDomainData(data);
-      //store f0
-      const freq = yin(data, sampleRate);
-      if(freq < that.maxFreq && freq > that.minFreq) {
-        that.micLabel.innerHTML = freq.toFixed(2);
-        that.ready();
-      }
-    }
-
-    this.share.addTimeout(setInterval(intervalFn, this.measurementInterval));
-    this.share.addTimeout(setTimeout(stop, this.micDuration*1000));
+    this.nextButton.onclick = function() {
+      uploadProgress(that.share.get('id'), 'intro','completed');
+      manager.next();
+    };
   }
 
   start() {
